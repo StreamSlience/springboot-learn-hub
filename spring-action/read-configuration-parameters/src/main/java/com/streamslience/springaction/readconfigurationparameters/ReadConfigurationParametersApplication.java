2758058fe3d609a44fe24a75c3bc5bb1b4ad2083
StreamSlience <1452909091@qq.com>
2020-06-26 10:42:42 +0800

    优化

--- spring-action/read-configuration-parameters/src/main/java/com/streamslience/springaction/readconfigurationparameters/ReadConfigurationParametersApplication.java
+++ spring-action/read-configuration-parameters/src/main/java/com/streamslience/springaction/readconfigurationparameters/ReadConfigurationParametersApplication.java
@@ -2,8 +2,10 @@ package com.streamslience.springaction.readconfigurationparameters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({WithConfigurationProperties.class})
 public class ReadConfigurationParametersApplication {
 
     public static void main(String[] args) {
--- spring-action/read-configuration-parameters/src/main/java/com/streamslience/springaction/readconfigurationparameters/WithConfigurationProperties.java
+++ spring-action/read-configuration-parameters/src/main/java/com/streamslience/springaction/readconfigurationparameters/WithConfigurationProperties.java
@@ -8,13 +8,16 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 使用{@code @ConfigurationProperties}读取配置文件参数
+ * 使用{@code @ConfigurationProperties} 和 {@code @Configuration}/{@code @Component}同时作用于类读取配置文件参数并注册为bean对象。
+ *
+ * 当然还有另外一种形式，可以不使用{@code @Configuration}注入bean对象,
+ * 而是在启动类中使用{@code @EnableConfigurationProperties}来指定要生成的bean对象
  *
  * @author StreamSlience
  * @date 2020-06-25 14:35
  */
 @Data
-@Configuration
+//@Configuration
 @ConfigurationProperties(prefix = "library")
 public class WithConfigurationProperties {
 
