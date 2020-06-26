73d5b939ba65fb53f33089182d97f8013ec8e4df
StreamSlience <1452909091@qq.com>
2020-06-26 10:05:38 +0800

    新增springboot启动小尾巴

---
+++ spring-action/start-animation/src/main/java/com/streamslience/springaction/startanimation/StartAnimationApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.springaction.startanimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAnimationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartAnimationApplication.class, args);
+    }
+
+}
---
+++ spring-action/start-animation/src/test/java/com/streamslience/springaction/startanimation/StartAnimationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamslience.springaction.startanimation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartAnimationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
