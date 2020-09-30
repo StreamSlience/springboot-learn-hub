cbb2dfc36a951c0c3897c4fd2ab7d44c545623ae
StreamSlience <1452909091@qq.com>
2020-10-01 01:37:33 +0800

    修复异常

--- spring-action/map-struct/official-tutorial/src/main/java/com/streamslience/mapstruct/officialtutorial/retrievingamapper/mapper/IJsr330Mapper.java
+++ spring-action/map-struct/official-tutorial/src/main/java/com/streamslience/mapstruct/officialtutorial/retrievingamapper/mapper/IJsr330Mapper.java
@@ -6,6 +6,6 @@ import org.mapstruct.Mapper;
  * @author StreamSlience
  * @date 2020-09-29 15:26
  */
-@Mapper(componentModel = "jsr330",implementationName = "iJsr330Mapper")
+@Mapper(componentModel = "jsr330",implementationName = "iJsr330MapperImpl")
 public interface IJsr330Mapper extends IUserBasicMapper  {
 }
